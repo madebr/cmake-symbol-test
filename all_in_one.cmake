31eb376e26c8e22b403c76b75086da8def703859
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-12-10 23:27:02 +0100

    remove c++ test

--- all_in_one.cmake
+++ all_in_one.cmake
@@ -123,100 +123,6 @@ function(compare_check_include_file CHECKS)
     endif()
 endfunction()
 
-
-function(parallel_check_cxx_symbol_exists)
-    cmake_parse_arguments(ARG "" "" "CHECKS;HEADERS" ${ARGN})
-
-    set(src_hdr)
-    foreach(hdr IN LISTS ARG_HEADERS)
-        string(APPEND header_include_str "#include <${hdr}>\n")
-    endforeach()
-
-    list(LENGTH ARG_CHECKS arg_checks_length)
-
-    set(cpp_checks "")
-    set(cpp_main_body "")
-    set(list )
-
-    set(i 0)
-    while (i LESS arg_checks_length)
-        list(GET ARG_CHECKS "${i}" var)
-        math(EXPR i "${i} + 1")
-        list(GET ARG_CHECKS "${i}" symbol)
-        math(EXPR i "${i} + 1")
-        list(GET ARG_CHECKS "${i}" first)
-        math(EXPR i "${i} + 1")
-        list(GET ARG_CHECKS "${i}" count)
-        math(EXPR i "${i} + 1")
-
-        if(DEFINED CACHE{${var}})
-            continue()
-        endif()
-
-        list(APPEND list "${var}" "${symbol}" "${first}" "${var}")
-
-        message(STATUS "Performing Parallel Test ${var}")
-
-        set(extra_args)
-        set(a 1)
-        while(a LESS ${count})
-            string(APPEND extra_args ", 0")
-            math(EXPR a "${a}+1")
-        endwhile()
-
-        string(APPEND cpp_checks "void ${symbol}() = delete;\ntemplate<typename T> concept Has_${symbol} = requires (T t) { ::${symbol}(t${extra_args}); };\nconstexpr const char *has_${symbol} = Has_${symbol}<${first}> ? \"INFO[${var}=1]\" : \"INFO[${var}=0]\";\n\n")
-        string(APPEND cpp_main_body "  result += has_${symbol}[argc];\n")
-    endwhile()
-
-    list(LENGTH list length_list)
-    if(length_list EQUAL 0)
-        return()
-    endif()
-    string(MD5 md5_list "${list}")
-
-    set(src "${CMAKE_CURRENT_BINARY_DIR}/${md5_list}_parallel_checks.cpp")
-    set(bin "${CMAKE_CURRENT_BINARY_DIR}/${md5_list}_parallel_checks_bin")
-
-    file(WRITE "${src}" "${header_include_str}\n${cpp_checks}int main(int argc, char *argv[]) {\n  int result = 0;  \n  (void)argv;\n${cpp_main_body}  return result;\n}\n")
-    try_compile(COMPILED "${CMAKE_CURRENT_BINARY_DIR}/tt" SOURCES "${src}"
-        COPY_FILE "${bin}"
-        CXX_STANDARD 20
-    )
-    if(NOT COMPILED)
-        message(FATAL_ERROR "FIXME: Failure to build -> fall back")
-    endif()
-
-    set(i 0)
-    while(i LESS length_list)
-        list(GET list "${i}" var)
-        math(EXPR i "${i} + 1")
-        list(GET list "${i}" symbol)
-        math(EXPR i "${i} + 1")
-        list(GET list "${i}" first)
-        math(EXPR i "${i} + 1")
-        list(GET list "${i}" count)
-        math(EXPR i "${i} + 1")
-
-        set(re "INFO\\[${var}=(1|0)\\]")
-        file(STRINGS "${bin}" has_var REGEX "${re}")
-
-        if(NOT has_var)
-            continue()
-        endif()
-
-        string(REGEX MATCH "${re}" yes_no "${has_var}")
-        if(CMAKE_MATCH_1)
-            message(STATUS "Performing Parallel Test ${var} - Success")
-            set(v "1")
-        else()
-            message(STATUS "Performing Parallel Test ${var} - Failed")
-            set(v "")
-        endif()
-
-        set("${var}" "${v}" CACHE INTERNAL "Have include ${header}")
-    endwhile()
-endfunction()
-
 include(CheckSymbolExists)
 include(CheckCSourceCompiles)
 
@@ -264,7 +170,6 @@ function(compare_check_symbol_checks)
     endif()
 
     list(LENGTH ARG_CHECKS arg_checks_length)
-    set(issues_cxx)
     set(issues_linker)
 
     foreach(symbol IN LISTS ARG_SYMBOLS)
@@ -273,23 +178,14 @@ function(compare_check_symbol_checks)
         set(varname "${ARG_VAR_PREFIX}${varname_core}${ARG_VAR_SUFFIX}")
 
         set(ref "${${varname}_REFERENCE}")
-        set(got_cxx "${${varname}}")
         set(got_linker "${${varname}_LINKER}")
 
-        if(NOT ref STREQUAL got_cxx)
-            list(APPEND issues_cxx ${varname})
-        endif()
         if(NOT ref STREQUAL got_linker)
             list(APPEND issues_linker ${varname})
         endif()
 
-        message(STATUS "ref=${ref} got_cxx=${got} got_linker=${got_linker}          ${varname}")
+        message(STATUS "ref=${ref} got_linker=${got_linker}          ${varname}")
     endforeach()
-    if(issues_cxx)
-        message(WARNING "These symbol checks don't match using CXX: ${issues_cxx}")
-    else()
-        message(STATUS "All CXX symbol checks match!")
-    endif()
     if(issues_linker)
         message(WARNING "These symbol checks don't match using LINKER: ${issues_linker}")
     else()
